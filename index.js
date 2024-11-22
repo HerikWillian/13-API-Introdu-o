c497e38d8a103713ef6f3b0a177a3e20028ce94f
HerikWillian <166548471+HerikWillian@users.noreply.github.com>
2024-11-22 22:30:59 +0000

    pronto

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-import express from "express";      // Requisição do pacote do express
-const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+import dotenv from "dotenv"; // Carregar variáveis de ambiente
+import express from "express"; // Requisição do pacote express
+import { selectUsuarios } from "./bd.js"; // Importa o método selectUsuarios
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
-  console.log("Rota GET/ solicitada");
+dotenv.config(); // Carrega variáveis de ambiente do arquivo .env
+
+const app = express(); // Instancia o Express (sem parâmetros)
+const port = process.env.PORT || 3000; // Porta definida via variável de ambiente ou padrão 3000
+
+// Rota da raiz do projeto
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
   res.json({
-    nome: "Herik Willian",      // Substitua pelo seu nome
+    nome: "Herik Willian Nogueira Soares e Yury Gabriel", // Substitua pelo seu nome
   });
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço escutando na porta:  ${port}`);
+// Rota /usuarios para buscar usuários
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await selectUsuarios(); // Busca os usuários
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+
+  console.log("Rota GET/usuarios solicitada");
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Serviço escutando na porta: ${port}`);
 });
