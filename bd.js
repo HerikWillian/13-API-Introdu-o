ae87135d713ac08acec89b20da4dfe8b789cb006
HerikWillian <166548471+HerikWillian@users.noreply.github.com>
2025-02-03 23:32:15 +0000

    pronto

--- bd.js
+++ bd.js
@@ -30,3 +30,38 @@ async function selectUsuario(id) {
 
 //bd.js
 export { selectUsuarios, selectUsuario };
+
+//bd.js
+async function insertUsuario(data) {
+  const client = await connect();
+  const query = "INSERT INTO usuario (nome,senha,email) VALUES ($1,$2,$3) ";
+  const usuario = [data.nome, data.senha, data.email];
+  await client.query(query, usuario);
+  client.release();
+}
+
+//bd.js
+export { selectUsuarios, selectUsuario, insertUsuario };
+
+//bd.js
+async function deleteUsuario(id) {
+  const client = await connect();
+  const query = "DELETE FROM usuario WHERE id = $1";
+  await client.query(query, [id]);
+  client.release();
+}
+
+//bd.js
+export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario };
+
+//bd.js
+async function updateUsuario(id, data) {
+  const client = await connect();
+  const query = "UPDATE usuario SET nome = $1, email = $2, senha = $3 WHERE id = $4";
+  const usuario = [data.nome, data.email, data.senha, id];
+  await client.query(query, usuario);
+  client.release();
+}
+
+//bd.js
+export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario };
