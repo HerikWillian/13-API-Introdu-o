bf90ad7ecb01171016469b67834383bdc4dd3b7b
HerikWillian <166548471+HerikWillian@users.noreply.github.com>
2025-02-03 23:10:28 +0000

    quase lá

--- bd.js
+++ bd.js
@@ -17,3 +17,16 @@ async function selectUsuarios() {
 }
 
 export { selectUsuarios };
+
+//bd.js
+async function selectUsuario(id) {
+  const client = await connect();
+  const query = "SELECT * FROM usuario WHERE id = $1";
+  const usuario = [id];
+  const res = await client.query(query, usuario);
+  client.release();
+  return res.rows;
+}
+
+//bd.js
+export { selectUsuarios, selectUsuario };
